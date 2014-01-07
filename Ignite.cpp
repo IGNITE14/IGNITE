47d7fd99d276d5237a355183d80aca92d3705afe
IGNITE14 <ignite1414@gmail.com>
2014-01-07 15:05:15 +0100

    Added Version Control

--- Ignite.cpp
+++ Ignite.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#define VERSION 0.1
+#define SUBVERSION "a"
 
 void ignite(){
-    cout << "Welcome to Ignite!" << endl;
+    cout << "Welcome to Ignite version " << VERSION << SUBVERSION << << endl;
     mainmenu();
 }
 
